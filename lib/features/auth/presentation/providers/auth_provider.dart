9b51a0a926e81ea3daa5b020524cfef63cbdcb05
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-17 22:41:07 -0500

    login logout user provider

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -5,14 +5,25 @@ import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
 
 final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
   final authRepository = AuthRepositoryImp();
-  return AuthNotifier();
+  return AuthNotifier( authRepository: authRepository );
 });
 
 class AuthNotifier extends StateNotifier< AuthState > {
-  AuthNotifier(): super( AuthState() );
+  final AuthRepository authRepository;
+  AuthNotifier({
+    required this.authRepository
+  }): super( AuthState() );
 
   void loginUser( String email, String password ) async {
-
+    await Future.delayed( const Duration(milliseconds: 500) );
+    try {
+      final user = await authRepository.login(email, password);
+      _setLoggedUser(user);
+    } on WrongCredentials {
+      logout( 'Invalid credentials' );
+    } catch (e) {
+      logout( 'Error not controlled' );
+    }
   }
 
   void registerUser( String email, String password ) async {
@@ -21,6 +32,23 @@ class AuthNotifier extends StateNotifier< AuthState > {
 
   void checkAuthStatus( ) async {
     
+  }
+
+  void _setLoggedUser( User user  ) async {
+    // todo need save token in device
+    state = state.copyWith(
+      user: user,
+      authStatus: AuthStatus.authenticated,
+    );
+  }
+
+  Future<void> logout ( [ String? errorMessage ] ) async {
+     // TODO: clean token
+     state = state.copyWith(
+      authStatus: AuthStatus.notAuthenticated,
+      user: null,
+      errorMessage: errorMessage,
+    );
   }
   
 }
@@ -39,11 +67,11 @@ class AuthState {
     this.errorMessage = '',
   });
 
-  AuthState copyWith(
+  AuthState copyWith({
     AuthStatus? authStatus,
     User? user,
     String? errorMessage,
-  ) => AuthState(
+  }) => AuthState(
     authStatus: authStatus ?? this.authStatus,
     user: user ?? this.user,
     errorMessage: errorMessage ?? this.errorMessage,
