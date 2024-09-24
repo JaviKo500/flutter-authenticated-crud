61506be4b5fed9c3914d057ee62b456096c4540f
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-23 23:00:27 -0500

    check status auth

--- lib/features/auth/infrastructure/datasources/auth_datasource_imp.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_imp.dart
@@ -7,9 +7,29 @@ class AuthDatasourceImpl extends AuthDatasource {
   final dio = Dio(BaseOptions(baseUrl: Environment.apiUrl));
 
   @override
-  Future<User> checkAuthStatus(String token) {
-    // TODO: implement checkAuthStatus
-    throw UnimplementedError();
+  Future<User> checkAuthStatus(String token) async {
+    try {
+      final response = await dio
+          .get('/auth/check-status',
+            options: Options(
+              headers: {
+                'Authorization': 'Bearer $token'
+              }
+            )
+          );
+      final user = UserMapper.userJsonToEntity(response.data);
+      return user;
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) {
+        throw CustomError('Invalid token');
+      }
+      if (e.type == DioExceptionType.connectionTimeout) {
+        throw CustomError('Review your internet connection');
+      }
+      throw Exception('Something wrong happened');
+    } catch (e) {
+      throw Exception('Something wrong happened');
+    }
   }
 
   @override
--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -2,17 +2,27 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/auth/domain/domain.dart';
 import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service_impl.dart';
 
 final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
   final authRepository = AuthRepositoryImp();
-  return AuthNotifier( authRepository: authRepository );
+  final keyValueStorageService =  KeyValueStorageServiceImpl();
+  return AuthNotifier( 
+    authRepository: authRepository,
+    keyValueStorageService: keyValueStorageService,
+  );
 });
 
 class AuthNotifier extends StateNotifier< AuthState > {
   final AuthRepository authRepository;
+  final KeyValueStorageService keyValueStorageService;
   AuthNotifier({
-    required this.authRepository
-  }): super( AuthState() );
+    required this.authRepository,
+    required this.keyValueStorageService,
+  }): super( AuthState() ){
+    checkAuthStatus();
+  }
 
   Future<void> loginUser( String email, String password ) async {
     await Future.delayed( const Duration(milliseconds: 500) );
@@ -32,10 +42,19 @@ class AuthNotifier extends StateNotifier< AuthState > {
 
   void checkAuthStatus( ) async {
     
+    final token = await keyValueStorageService.getValue<String>('token');
+    if( token == null || token.isEmpty ) return logout();
+
+    try {
+      final user = await authRepository.checkAuthStatus(token);
+      _setLoggedUser(user);
+    } catch (e) {
+      logout();
+    }
   }
 
   void _setLoggedUser( User user  ) async {
-    // todo need save token in device
+    await keyValueStorageService.setKeyValue('token', user.token);
     state = state.copyWith(
       user: user,
       authStatus: AuthStatus.authenticated,
@@ -44,8 +63,8 @@ class AuthNotifier extends StateNotifier< AuthState > {
   }
 
   Future<void> logout ( [ String? errorMessage ] ) async {
-     // TODO: clean token
-     state = state.copyWith(
+    await keyValueStorageService.removeValue('token');
+    state = state.copyWith(
       authStatus: AuthStatus.notAuthenticated,
       user: null,
       errorMessage: errorMessage,
