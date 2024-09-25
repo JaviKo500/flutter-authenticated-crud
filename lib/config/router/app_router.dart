e70e1397ddce6508b44e1238678b3edcbac079ea
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-24 22:42:02 -0500

    rebuild and app route notifier

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,11 +1,14 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:teslo_shop/config/router/app_router_notifier.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
 final goRouterProvider = Provider((ref) {
+  final goRouterNotifier = ref.read( goRouterNotifierProvider );
   return GoRouter(
     initialLocation: '/splash',
+    refreshListenable: goRouterNotifier,
     routes: [
       ///* First screen
       GoRoute(
@@ -30,6 +33,7 @@ final goRouterProvider = Provider((ref) {
       ),
     ],
     redirect: (context, state) {
+      print(state.subloc);
       return '/login';
     },
   );
