e17de0c66fea673f276e71a45dd5c96406790353
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-24 22:57:59 -0500

    validate auth routes

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teslo_shop/config/router/app_router_notifier.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
+import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
 final goRouterProvider = Provider((ref) {
@@ -33,8 +34,21 @@ final goRouterProvider = Provider((ref) {
       ),
     ],
     redirect: (context, state) {
-      print(state.subloc);
-      return '/login';
+      final isGoingTo = state.subloc;
+      final authStatus = goRouterNotifier.authStatus;
+      if( isGoingTo == '/splash' && authStatus == AuthStatus.checking ) return null;
+
+      if ( authStatus == AuthStatus.notAuthenticated ) {
+        if( isGoingTo == '/login' || isGoingTo == '/register') return null;
+        return '/login';
+      }
+
+      if ( authStatus == AuthStatus.authenticated ) {
+        if( isGoingTo == '/login' || isGoingTo == '/register' || isGoingTo == '/splash') return '/';
+      }
+
+
+      return null;
     },
   );
 });
