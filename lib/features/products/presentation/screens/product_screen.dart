90100fc36a06d1e8d6ae3169b6f6615580875b8a
JaViKo500 <javikogutierrez64@gmail.com>
2024-10-13 12:09:58 -0500

    connect provider with  form

--- lib/features/products/presentation/screens/product_screen.dart
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -1,7 +1,8 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/products/domain/domain.dart';
-import 'package:teslo_shop/features/products/presentation/providers/product_provider.dart';
+import 'package:teslo_shop/features/products/presentation/providers/providers.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
 
@@ -41,15 +42,15 @@ class ProductScreen extends ConsumerWidget {
 }
 
 
-class _ProductView extends StatelessWidget {
+class _ProductView extends ConsumerWidget {
 
   final Product product;
 
   const _ProductView({required this.product});
 
   @override
-  Widget build(BuildContext context) {
-
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productForm = ref.watch( productFormProvider( product ) );
     final textStyles = Theme.of(context).textTheme;
 
     return ListView(
@@ -58,13 +59,13 @@ class _ProductView extends StatelessWidget {
           SizedBox(
             height: 250,
             width: 600,
-            child: _ImageGallery(images: product.images ),
+            child: _ImageGallery(images: productForm.images ),
           ),
     
           const SizedBox( height: 10 ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Center(child: Text( product.title, style: textStyles.titleSmall, textAlign: TextAlign.center, )),
+            child: Center(child: Text( productForm.title.value, style: textStyles.titleSmall, textAlign: TextAlign.center, )),
           ),
           const SizedBox( height: 10 ),
           _ProductInformation( product: product ),
@@ -82,7 +83,7 @@ class _ProductInformation extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref ) {
 
-    
+    final productForm = ref.watch( productFormProvider( product ) );
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -94,26 +95,32 @@ class _ProductInformation extends ConsumerWidget {
           CustomProductField( 
             isTopField: true,
             label: 'Nombre',
-            initialValue: product.title,
+            initialValue: productForm.title.value,
+            onChanged: ref.read( productFormProvider( product ).notifier ).onTitleChange,
+            errorMessage: productForm.title.errorMessage,
           ),
           CustomProductField( 
             isTopField: true,
             label: 'Slug',
-            initialValue: product.slug,
+            initialValue: productForm.slug.value,
+            onChanged: ref.read( productFormProvider( product ).notifier ).onSlugChange,
+            errorMessage: productForm.slug.errorMessage,
           ),
           CustomProductField( 
             isBottomField: true,
             label: 'Precio',
             keyboardType: const TextInputType.numberWithOptions(decimal: true),
-            initialValue: product.price.toString(),
+            initialValue: productForm.price.value.toString(),
+            onChanged:( value ) =>  ref.read( productFormProvider( product ).notifier ).onPriceChange( double.tryParse(value) ?? 0 ),
+            errorMessage: productForm.price.errorMessage,
           ),
 
           const SizedBox(height: 15 ),
           const Text('Extras'),
 
-          _SizeSelector(selectedSizes: product.sizes ),
+          _SizeSelector(selectedSizes: productForm.sizes ),
           const SizedBox(height: 5 ),
-          _GenderSelector( selectedGender: product.gender ),
+          _GenderSelector( selectedGender: productForm.gender ),
           
 
           const SizedBox(height: 15 ),
@@ -121,7 +128,9 @@ class _ProductInformation extends ConsumerWidget {
             isTopField: true,
             label: 'Existencias',
             keyboardType: const TextInputType.numberWithOptions(decimal: true),
-            initialValue: product.stock.toString(),
+            initialValue: productForm.inStock.value.toString(),
+            onChanged: (value) => ref.read( productFormProvider( product ).notifier ).onStockChange( int.tryParse(value) ?? 0 ),
+            errorMessage: productForm.inStock.errorMessage,
           ),
 
           CustomProductField( 
@@ -136,7 +145,7 @@ class _ProductInformation extends ConsumerWidget {
             maxLines: 2,
             label: 'Tags (Separados por coma)',
             keyboardType: TextInputType.multiline,
-            initialValue: product.tags.join(', '),
+            initialValue: productForm.tags
           ),
 
 
