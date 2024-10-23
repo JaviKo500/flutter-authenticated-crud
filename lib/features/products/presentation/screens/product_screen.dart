adcef2008e54dbe6c5fec5cb47c89dd5f14adf58
JaViKo500 <javikogutierrez64@gmail.com>
2024-10-22 23:53:25 -0500

    delete log

--- lib/features/products/presentation/screens/product_screen.dart
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -286,7 +286,6 @@ class _ImageGallery extends StatelessWidget {
         viewportFraction: 0.7
       ),
       children: images.map((image){
-        print(image);
         late ImageProvider imageProvider;
         if ( image.startsWith('http') ) {
           imageProvider = NetworkImage(image);
